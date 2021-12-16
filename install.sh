44b1c7f3c1168b69136162dab6fe6ec926868097
Höhl, Lukas <lukas.hoehl@accso.de>
2021-12-16 14:42:15 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+echo "Installing lunarvim"
+git clone --depth 1 https://github.com/LunarVim/LunarVim.git $HOME/.config/nvim &
+
+binaries=( go docker podman neovim )
+head_binaries=( colima )
+
+echo "Fetching binaries"
+for binary in "${binaries[@]}"; do
+  echo "fetching $binary"
+  brew fetch $binary &
+done
+
+echo "Fetching head binaries"
+for binary in "${head_binaries[@]}"; do
+  echo "fetching $binary"
+  brew fetch $binary &
+done
+
+for job in $(jobs -p); do
+  wait $job
+done
+
+echo "Installing Brew Binaries"
+brew install $binaries
+
+echo "Installing casks"
+brew install --cask goneovim 
+
+echo "Installing head binaries"
+brew install --HEAD $head_binaries
+
+echo "Installing pip packages"
+pip install pynvim &
 
 for dir in $(/bin/ls -d */); do
   echo "Stowing $dir"
