6f4761b14047a2a897a9952c428ee5f2bfcbcc65
Höhl, Lukas <lukas.hoehl@accso.de>
2022-07-22 14:23:57 +0200

    go format
    Signed-off-by: Höhl, Lukas <lukas.hoehl@accso.de>

--- nvim/plugins/configs/go.lua
+++ nvim/plugins/configs/go.lua
@@ -1,11 +1,2 @@
 require("go").setup({})
--- local autocmd = vim.api.nvim_create_autocmd
---
--- autocmd("BufWritePre", {
---   pattern = "*.go",
---   callback = function()
---     require('go.format').goimport()
---   end,
--- })
-
--- vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').gofmt() ]], false)
+vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
